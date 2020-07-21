0b45bdd31ec2f45776dbfa0233c88db35efc561d
Shashank Sharma <shashank.sharma98@gmail.com>
2020-07-22 02:38:13 +0530

    Loader added for homepage only

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,15 @@
-window.onload = function() {
+window.onload = function () {
   const footer_portal = document.getElementById("footer-portal");
   const footer_expand_portal = document.getElementById("footer-expand-portal");
   const footer_expand_content = document.getElementById("footer-expand-content");
+  const loadingPage = document.getElementById("loading-page");
+
+  const height = window.innerHeight;
+  const width = window.innerWidth;
+  loadingPage.style.setProperty("--width", width);
+  loadingPage.style.setProperty("--height", height);
+
+  loadingPage.classList.add("collapse");
 
   document.addEventListener('keydown', (event) => {
     const keyName = event.key;
@@ -25,11 +33,11 @@ window.onload = function() {
     }
   }, false);
 
-  footer_portal.onclick = function() {
+  footer_portal.onclick = function () {
     footer_portal.style.animation = "spin 10s cubic-bezier(0.11,0,0.82,0.73), spin 8s linear 10s infinite";
     footer_portal.style.top = "-60vh";
     footer_expand_portal.style.height = "100vh";
-    setTimeout( () => {
+    setTimeout(() => {
       footer_expand_content.style.display = 'block';
       footer_expand_content.style.opacity = "1";
     }, 1000)
