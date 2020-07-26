a4c3155a9d98bcfef2a5cd486d55dafb69710ed5
Shashank Sharma <shashank.sharma98@gmail.com>
2020-07-26 18:42:59 +0530

    Timeline hide bug fixed, fix color palette, font-size for overflow

--- public/js/main.js
+++ public/js/main.js
@@ -31,6 +31,7 @@ window.onload = function () {
   // const open_timer = document.getElementById("open-timer");
   // let open_timer_func = null;
   let header_timer_func = null;
+  let timeline = null;
   let page_state = "home";
   let header_state = "default";
 
@@ -40,6 +41,8 @@ window.onload = function () {
   const certification_indicator = document.getElementById("certification-indicator");
   const volunteer_indicator = document.getElementById("volunteer-indicator");
   const project_indicator = document.getElementById("project-indicator");
+
+  let timeline_hide = {};
 
 
   // Create a DataSet (allows two way data-binding)
@@ -102,36 +105,195 @@ window.onload = function () {
       content: '<span style="font-weight:600;">Dec 12</span><br/>Google Code-in 2015',
       start: "2015-12-12", end: "2016-03-01", className: "volunteer"
     },
-    { id: 0, content: '<span style="font-weight:600;">May 09</span><br/>College started', start: "2016-05-09", className: "event no-end" },
-    { id: "competitive-codechef-start", content: '<span style="font-weight:600;">Aug 12</span><br/>Codechef | competitive coding', start: "2016-08-12", end: "2016-12-31", className: "project" },
-    { id: "competitive-hackerearth-start", content: '<span style="font-weight:600;">Oct 01</span><br/>Hackerearth', start: "2016-10-01", end: "2017-10-10", className: "project" },
+    {
+      id: 0,
+      content: '<span style="font-weight:600;">May 09</span><br/>College started',
+      start: "2016-05-09",
+      className: "event no-end"
+    },
+    {
+      id: "competitive-codechef-start",
+      content: '<span style="font-weight:600;">Aug 12</span><br/>Codechef | competitive coding',
+      start: "2016-08-12",
+      end: "2016-12-31",
+      className: "project"
+    },
+    {
+      id: "competitive-hackerearth-start",
+      content: '<span style="font-weight:600;">Oct 01</span><br/>Hackerearth',
+      start: "2016-10-01",
+      end: "2017-10-10",
+      className: "project"
+    },
     // Aug 3, 2017
-    { id: "internship-1", content: '<span style="font-weight:600;">Aug 03</span><br/>Newrubric', start: "2017-08-03", end: "2018-03-12", className: "internship" },
-    { id: "internship-2", content: '<span style="font-weight:600;">Nov 20</span><br/>Teknuance', start: "2017-11-20", end: "2018-05-08", className: "internship" },
-    { id: "internship-3", content: '<span style="font-weight:600;">Feb 02</span><br/>AudienceSutra', start: "2018-02-06", end: "2018-08-15", className: "internship" },
-    { id: "internship-4", content: '<span style="font-weight:600;">Jun 07</span><br/>YourDOST', start: "2018-06-07", end: "2018-09-25", className: "internship" },
-    { id: "internship-5", content: '<span style="font-weight:600;">Sep 03</span><br/>AbsoluteFace', start: "2018-09-03", end: "2018-11-01", className: "internship" },
-    { id: "internship-6", content: '<span style="font-weight:600;">Oct 23</span><br/>Omoads', start: "2018-10-23", end: "2018-12-22", className: "internship" },
-    { id: "internship-7", content: '<span style="font-weight:600;">May 05</span><br/>Axcelorate Labs pvt ltd', start: "2019-05-05", end: "2019-12-02", className: "internship" },
-    { id: "internship-8", content: '<span style="font-weight:600;">Dec 09</span><br/>Atlan', start: "2019-12-09", end: "2020-06-20", className: "internship" },
-    { id: "certification-0", content: '<span style="font-weight:600;">Jan 11</span><br/>NPTEL: Introduction to Python', start: "2017-01-11", end: "2017-6-15", className: "certification" },
-    { id: "certification-1", content: '<span style="font-weight:600;">Sep 01</span><br/>CS50: Harvard University', start: "2017-09-01", end: "2018-01-10", className: "certification" },
-    { id: "certification-2", content: '<span style="font-weight:600;">Feb 01</span><br/>Udacity: Google India Challenge Scholarship', start: "2018-02-01", className: "certification no-end" },
-    { id: "certification-3", content: '<span style="font-weight:600;">Aug 14</span><br/>Infosys Certified', start: "2019-08-14", className: "certification no-end" },
-    { id: "volunteer-1", content: '<span style="font-weight:600;">May 01</span><br/>College: Technical Club', start: "2018-05-01", className: "volunteer no-end" },
-    { id: "volunteer-2", content: '<span style="font-weight:600;">Aug 01</span><br/>DSC: Project Lead', start: "2019-08-01", end: "2020-06-01", className: "volunteer" },
-    { id: "volunteer-3", content: '<span style="font-weight:600;">Aug 14</span><br/>DjangoGirls: Mentor', start: "2019-08-14", className: "volunteer no-end" },
-    { id: "volunteer-4", content: '<span style="font-weight:600;">Feb 22</span><br/>Isohack: Hackathon organized', start: "2020-02-22", className: "volunteer no-end" },
-    { id: "volunteer-5", content: '<span style="font-weight:600;">Nov 01</span><br/>DjangoGirls: Co-organizer', start: "2019-11-01", className: "volunteer no-end" },
-    { id: "volunteer-6", content: '<span style="font-weight:600;">Feb 01</span><br/>GirlScript Summer of Code', start: "2020-02-01", end: "2020-06-01", className: "volunteer" },
-    { id: "hackathon-1", content: '<span style="font-weight:600;">Mar 28</span><br/>Smart India Hackathon 2018', start: "2018-03-28", className: "hackathon no-end" },
-    { id: "hackathon-2", content: '<span style="font-weight:600;">Sep 29</span><br/>NITI: Pune Hackathon', start: "2018-09-29", className: "hackathon no-end" },
-    { id: "hackathon-3", content: '<span style="font-weight:600;">Oct 30</span><br/>HackAcro | Winner', start: "2018-10-30", className: "hackathon no-end" },
-    { id: "hackathon-4", content: '<span style="font-weight:600;">Apr 05</span><br/>Void Hacks, Indore', start: "2019-04-05", className: "hackathon no-end" },
-    { id: "hackathon-5", content: '<span style="font-weight:600;">Aug 26</span><br/>College: Winner, Project Exhibition', start: "2019-8-26", className: "hackathon no-end" },
-    { id: "hackathon-6", content: '<span style="font-weight:600;">Oct 04</span><br/>HackIndore 2.0', start: "2019-10-04", className: "hackathon no-end" },
-    { id: "hackathon-7", content: '<span style="font-weight:600;">Oct 11</span><br/>Version Beta 2.0, Bhopal', start: "2019-10-11", className: "hackathon no-end" },
-    { id: "project-1", content: '<span style="font-weight:600;">Jan 02</span><br/>#100DaysOfCode', start: "2017-01-02", className: "volunteer no-end" },
+    {
+      id: "internship-1",
+      content: '<span style="font-weight:600;">Aug 03</span><br/>Newrubric',
+      start: "2017-08-03",
+      end: "2018-03-12",
+      className: "internship"
+    },
+    {
+      id: "internship-2",
+      content: '<span style="font-weight:600;">Nov 20</span><br/>Teknuance',
+      start: "2017-11-20",
+      end: "2018-05-08",
+      className: "internship"
+    },
+    {
+      id: "internship-3",
+      content: '<span style="font-weight:600;">Feb 02</span><br/>AudienceSutra',
+      start: "2018-02-06",
+      end: "2018-08-15",
+      className: "internship"
+    },
+    {
+      id: "internship-4",
+      content: '<span style="font-weight:600;">Jun 07</span><br/>YourDOST',
+      start: "2018-06-07",
+      end: "2018-09-25",
+      className: "internship"
+    },
+    {
+      id: "internship-5",
+      content: '<span style="font-weight:600;">Sep 03</span><br/>AbsoluteFace',
+      start: "2018-09-03",
+      end: "2018-11-01",
+      className: "internship"
+    },
+    {
+      id: "internship-6",
+      content: '<span style="font-weight:600;">Oct 23</span><br/>Omoads',
+      start: "2018-10-23",
+      end: "2018-12-22",
+      className: "internship"
+    },
+    {
+      id: "internship-7",
+      content: '<span style="font-weight:600;">May 05</span><br/>Axcelorate Labs pvt ltd',
+      start: "2019-05-05",
+      end: "2019-12-02",
+      className: "internship"
+    },
+    {
+      id: "internship-8",
+      content: '<span style="font-weight:600;">Dec 09</span><br/>Atlan',
+      start: "2019-12-09",
+      end: "2020-06-20",
+      className: "internship"
+    },
+    {
+      id: "certification-0",
+      content: '<span style="font-weight:600;">Jan 11</span><br/>NPTEL: Introduction to Python',
+      start: "2017-01-11",
+      end: "2017-6-15",
+      className: "certification"
+    },
+    {
+      id: "certification-1",
+      content: '<span style="font-weight:600;">Sep 01</span><br/>CS50: Harvard University',
+      start: "2017-09-01",
+      end: "2018-01-10",
+      className: "certification"
+    },
+    {
+      id: "certification-2",
+      content: '<span style="font-weight:600;">Feb 01</span><br/>Udacity: Google India Challenge Scholarship',
+      start: "2018-02-01",
+      className: "certification no-end"
+    },
+    {
+      id: "certification-3",
+      content: '<span style="font-weight:600;">Aug 14</span><br/>Infosys Certified',
+      start: "2019-08-14",
+      className: "certification no-end"
+    },
+    {
+      id: "volunteer-1",
+      content: '<span style="font-weight:600;">May 01</span><br/>College: Technical Club',
+      start: "2018-05-01",
+      className: "volunteer no-end"
+    },
+    {
+      id: "volunteer-2",
+      content: '<span style="font-weight:600;">Aug 01</span><br/>DSC: Project Lead',
+      start: "2019-08-01",
+      end: "2020-06-01",
+      className: "volunteer"
+    },
+    {
+      id: "volunteer-3",
+      content: '<span style="font-weight:600;">Aug 14</span><br/>DjangoGirls: Mentor',
+      start: "2019-08-14",
+      className: "volunteer no-end"
+    },
+    {
+      id: "volunteer-4",
+      content: '<span style="font-weight:600;">Feb 22</span><br/>Isohack: Hackathon organized',
+      start: "2020-02-22",
+      className: "volunteer no-end"
+    },
+    {
+      id: "volunteer-5",
+      content: '<span style="font-weight:600;">Nov 01</span><br/>DjangoGirls: Co-organizer',
+      start: "2019-11-01",
+      className: "volunteer no-end"
+    },
+    {
+      id: "volunteer-6",
+      content: '<span style="font-weight:600;">Feb 01</span><br/>GirlScript Summer of Code',
+      start: "2020-02-01",
+      end: "2020-06-01",
+      className: "volunteer"
+    },
+    {
+      id: "hackathon-1",
+      content: '<span style="font-weight:600;">Mar 28</span><br/>Smart India Hackathon 2018',
+      start: "2018-03-28",
+      className: "hackathon no-end"
+    },
+    {
+      id: "hackathon-2",
+      content: '<span style="font-weight:600;">Sep 29</span><br/>NITI: Pune Hackathon',
+      start: "2018-09-29",
+      className: "hackathon no-end"
+    },
+    {
+      id: "hackathon-3",
+      content: '<span style="font-weight:600;">Oct 30</span><br/>HackAcro | Winner',
+      start: "2018-10-30",
+      className: "hackathon no-end"
+    },
+    {
+      id: "hackathon-4",
+      content: '<span style="font-weight:600;">Apr 05</span><br/>Void Hacks, Indore',
+      start: "2019-04-05",
+      className: "hackathon no-end"
+    },
+    {
+      id: "hackathon-5",
+      content: '<span style="font-weight:600;">Aug 26</span><br/>College: Winner, Project Exhibition',
+      start: "2019-8-26",
+      className: "hackathon no-end"
+    },
+    {
+      id: "hackathon-6",
+      content: '<span style="font-weight:600;">Oct 04</span><br/>HackIndore 2.0',
+      start: "2019-10-04",
+      className: "hackathon no-end"
+    },
+    {
+      id: "hackathon-7",
+      content: '<span style="font-weight:600;">Oct 11</span><br/>Version Beta 2.0, Bhopal',
+      start: "2019-10-11",
+      className: "hackathon no-end"
+    },
+    {
+      id: "project-1",
+      content: '<span style="font-weight:600;">Jan 02</span><br/>#100DaysOfCode',
+      start: "2017-01-02",
+      className: "volunteer no-end"
+    },
   ]);
 
   // Configuration for the Timeline
@@ -141,24 +303,38 @@ window.onload = function () {
     zoomKey: "ctrlKey",
     width: "100%",
     height: "100vh",
-    start: new Date("January 21, 2014"),
-    end: new Date("January 21, 2019"),
+    start: new Date("January 01, 2015"),
+    end: new Date("May 21, 2018"),
     editable: false
   };
 
-  // Create a Timeline
-
-  function reveal_cards(type) {
+  function reveal_cards(type, toggle = true) {
     const card_list = document.getElementsByClassName(type);
-    if (card_list[0].style.opacity !== "0") {
-      for (let card of card_list) {
+    const indicator = document.getElementById(type + "-indicator");
+    const includeStatus = timeline_hide[type] || false;
+    let toggleStatus = false;
+
+    if (toggle) {
+      if (includeStatus) {
+        timeline_hide[type] = false;
+      } else if (!includeStatus) {
+        toggleStatus = true;
+        timeline_hide[type] = true;
+      }
+    } else if (includeStatus) {
+      toggleStatus = true;
+    }
+
+    for (let card of card_list) {
+      if (toggleStatus) {
+        indicator.style.textDecoration = "line-through";
         card.style.opacity = "0";
         setTimeout(() => {
           card.style.display = "none";
         }, 1000)
-      }
-    } else {
-      for (let card of card_list) {
+      } else {
+        indicator.style.textDecoration = "none";
+
         card.style.display = "block";
         setTimeout(() => {
           card.style.opacity = "1";
@@ -166,6 +342,17 @@ window.onload = function () {
       }
     }
   }
+
+  function timelineOnChange() {
+    timeline.on("rangechanged", function ({start, end}) {
+      console.log(timeline_hide);
+      // console.log(start.toString(), end.toString());
+      for (let type of Object.keys(timeline_hide)) {
+        reveal_cards(type, false);
+      }
+    });
+  }
+
   internship_indicator.onclick = function () {
     reveal_cards("internship");
   };
@@ -204,7 +391,7 @@ window.onload = function () {
   setTimeout(() => {
     loadingPage.style.zIndex = "0";
     home_header_portal.style.zIndex = "0";
-  }, 1000 );
+  }, 1000);
 
   home_header_portal.style.marginTop = "27vmin";
   home_header_portal.style.transform = "rotate3d(1, 0, 0, 65deg)";
@@ -336,10 +523,11 @@ window.onload = function () {
       home_timeline.style.display = "block";
       setTimeout(() => {
         home_timeline.style.opacity = "1";
-      }, 100 );
+      }, 100);
       if (home_timeline.style.opacity === "") {
         console.log("Creating timeline");
-        const timeline = new vis.Timeline(container, items, options);
+        timeline = new vis.Timeline(container, items, options);
+        timelineOnChange();
       }
     }, 1100)
   };
